a6ab919e28e47126dc93701689a686c70b4fb819
ero-senin <ayushpandey8522@gmail.com>
2017-03-25 23:32:54 +0530

    Updated code
    Changed representation of cube to 3D array, changed other functions accordingly

--- cube.cpp
+++ cube.cpp
@@ -30,29 +30,74 @@ Class:	cube
 		REMAINING ISSUES TO BE SOLVED LATER:
 			Every input that is valid and not solved is not solvable: need to think of additional validate checks!
 */
+
+// will rather write 9 rotation functions to generate next possible cube
+/* indexes on a face
+00 01 02
+10 11 12
+20 21 22
+*/
 #include <iostream>
+#include <string>
 using namespace std;
+#define WHITE 0
+#define RED 1
+#define GREEN 2
+#define ORANGE 3
+#define BLUE 4
+#define YELLOW 5
 class cube {
-	short state[6][9];		//the state of the cube is represented by a 6*9 array where every index stores the color of the jth cube of ith face
+	short state[6][3][3];		//the state of the cube is represented by a 6*3*3 array
+	//0:White(Front), 1:Red(Top), 2:Green(Right), 3:Orange(Bottom), 4:Blue(Left), 5:Yellow(Back)
+
+	string colors[]={"WHITE", "RED", "GREEN", "ORANGE", "BLUE", "YELLOW"};
 public:
 	
 	cube()					//constructor, takes input
 	{
+		cout<<"INSTRUCTIONS for placing the cube and feeding the states: \n Place the cube such that WHITE centerpeice is in front and the RED centerpeice is on top";		//Enter the instructions
+
+
 		for (int i = 0; i < 6; ++i)
 		{
-			cout<<"Enter the states for this face"<<endl;
-			for (int j = 0; j < 9; ++j)
+			cout<<"Enter the state for "<<colors[i]<<" face.";
+			for (int j = 0; j < 3; ++j)
 			{
-				cout<<"Enter the state of "<<j<<"th cube of "<<i<<"th face: ";
-				cin>>state[i][j];
+				for (int k = 0; k < 3; ++k)
+				{
+					if ((j==1)&&(k==1))		//centerpeice denotes the face
+					{
+						state[i][1][1]=i;
+					}
+					cout<<"Enter the state of peice at ( "<<j<<", "<<k<<" ) : ";
+				}
 			}
 		}
 	}
-	
-	int getState(int i, int j)		//returns state of a given cube
-	{
-		return state[i][j];
+
+	cube(short* arr) {
+		for (short i = 0; i < 6; i++) {
+			for (short j = 0; j < 6; j++) {
+				for (short k = 0; k < 6; k++) {
+					state[i][j][k] = arr[i][j][k];
+				}
+			}
+		}
 	}
+	bool validate();
+
+	cube nextPossible();
+
+	bool solved();
+
+	
+	
+	int getState(int i, int j, int k)		//returns state of a given cube
+	{
+		return state[i][j][k];
+	}
+
+
 };
 
 int main(int argc, char const *argv[])
@@ -60,9 +105,12 @@ int main(int argc, char const *argv[])
 	cube c;
 	for(int i=0; i<6; i++)
 	{
-		for (int j = 0; j < 9; ++j)
+		for (int j = 0; j < 3; ++j)
 		{
-			cout<<c.getState(i,j)<<" ";
+			for (int k = 0; k < 3; ++k)
+			{
+				cout<<c.getState(i, j, k);
+			}
 		}
 		cout<<endl;
 	}
